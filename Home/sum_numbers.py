6ddf6e3a4970a7a06cbf714490e96281fc18be89
franciscocruz29 <franciscohuertas25@gmail.com>
2022-06-02 17:15:20 -0500

    Solved Sum Numbers

---
+++ Home/sum_numbers.py
@@ -0,0 +1,33 @@
+""" def sum_numbers(text: str) -> int:
+    words = text.split()
+    count = 0
+    for word in words:
+        try:
+            count += int(word)
+        except ValueError:
+            pass
+    return count """
+
+sum_numbers = lambda text: sum(int(word) for word in text.split() if word.isdigit())
+
+# isdigit(), Check if all the characters in the text are digits
+
+if __name__ == "__main__":
+    print("Example:")
+    print(sum_numbers("hi"))
+
+    
+    assert sum_numbers("hi") == 0
+    assert sum_numbers("who is 1st here") == 0
+    assert sum_numbers("my numbers is 2") == 2
+    assert (
+        sum_numbers(
+            "This picture is an oil on canvas "
+            "painting by Danish artist Anna "
+            "Petersen between 1845 and 1910 year"
+        )
+        == 3755
+    )
+    assert sum_numbers("5 plus 6 is") == 11
+    assert sum_numbers("") == 0
+    print("Coding complete!")
