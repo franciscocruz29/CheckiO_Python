7f82042d8cd1414abccc1a248296e7dc9e5ec18a
franciscocruz29 <franciscohuertas25@gmail.com>
2022-07-27 10:16:32 -0500

    Completed All the same

---
+++ Home/all_the_same.py
@@ -0,0 +1,22 @@
+from typing import List, Any
+
+""" In this mission you should check if all elements in the given list are equal.
+
+Input: List.
+
+Output: Bool. """
+
+def all_the_same(elements: List[Any]) -> bool:
+    return len(set(elements)) <= 1
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(all_the_same([1, 1, 1]))
+
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete!")
